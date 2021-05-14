c8a1f7add327db943e27e3626532962f5a0100b2
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-05-14 22:09:16 +0300

    Добавлены классы фрагментов

---
+++ app/src/main/java/com/example/weirdcalculator/ui/CalendarFragment.java
@@ -0,0 +1,18 @@
+package com.example.weirdcalculator.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.weirdcalculator.R;
+
+public class CalendarFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return rootView;
+    }
+}
